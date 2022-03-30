96dc5720a15404d1fd15d5ec46be21c7248cbb7f
Frederic Perron <frederic.norrep@gmail.com>
2022-03-29 20:06:09 -0400

    added the table model (incomplete)

--- src/dollarify/db/__init__.py
+++ src/dollarify/db/__init__.py
@@ -5,6 +5,66 @@ from dollarify.static.db.sqlite3 import queries
 from dollarify import settings
 
 
+class TableModel():
+
+    # __slots__ = ('_name', 'column_sql')
+
+    def __init__(self, name: str, column_sql: str):
+        """
+        name: name of the table
+        columns_sql: columns part of the sql query referencing columns names and attributes. For instance, (uuid VARCHAR(32), name VARCHAR(32))
+        """
+        if len(name) < 1:
+            raise ValueError("The name of the table should be at least one character long.")
+
+        if len(column_sql) < 2 or not column_sql[0] == '(' or not column_sql[-1] == ')':
+            raise ValueError("The column_sql should respect the following format: (col_name1 VARCHAR(5), col_name2 DECIMAL(5,2), ...)")
+        self._name = name
+        self.column_sql = column_sql
+
+    @property
+    def name(self):
+        return self._name
+
+    def create():
+        pass
+
+    def drop():
+        pass
+
+    @property
+    def columns(self):
+        trimmed = self.column_sql[1:-1]
+        column_strings = trimmed.split(', ')
+        return {k.strip().split(' '):v for k, v in column_strings}
+    
+    @classmethod
+    def _parse_column_sql(cls, sql):
+        if sql[0] == '(' and sql[-1] == ')':
+            sql = sql[1:-1]
+        
+        try:
+            name, sql_attribs = sql.split(' ', 1)
+            sql_type, sql_flags = sql_attribs.strip().split(' ', 1)
+            return name, sql_type, sql_flags
+        except ValueError:
+            logging.error(f'The sql provided "{sql}" does not contain sql attributes.')
+    
+    @classmethod
+    def _parse_columns_sql(cls, sql):
+        pass
+
+    @property
+    def columns(self, names: bool, types: bool, flags: bool):
+        pass
+
+if __name__ == '__main__':
+    # t = TableModel('test', '(name VARCHAR(5), amount DECIMAL(5,2))')
+    test = TableModel._parse_column_sql('(col_name1 VARCHAR(5))')
+    print(test)
+    
+
+
 DB_MODULE, DB_CONNECTION, DB_CURSOR = None, None, None
 
 

57450bac09e3a8c18d465b185236337b745e2ddf
Frederic Perron <frederic.norrep@gmail.com>
2022-04-01 21:10:27 -0400

    change doc of init in static

--- src/dollarify/static/db/sqlite3/queries/__init__.py
+++ src/dollarify/static/db/sqlite3/queries/__init__.py
@@ -1,4 +1,4 @@
 """
 SQL Script files can contain multiple queries. 
-However, no result set can be retrieved from the queries and task can't be provided.
+However, no result set should be retrieved from the queries and task can't be provided.
 """

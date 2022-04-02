04ddadeed060dc0e61254f18ca49f5980c622b0a
Frederic Perron <frederic.norrep@gmail.com>
2022-04-01 21:26:16 -0400

    fixed the {table_name} in sql scripts

--- src/dollarify/__main__.py
+++ src/dollarify/__main__.py
@@ -1,9 +1,8 @@
-from ast import alias, arg
 import logging
 import sys
 import argparse
 
-from dollarify.db import Database, SQLiteDB
+from dollarify.db import Database, SQLiteDB, init
 from dollarify import settings
 
 
@@ -14,15 +13,15 @@ def main():
     args = sys.argv[1:]
     parser = argparse.ArgumentParser(prog='Dollarify', description='Manage Dollarify CLI')
     parser.add_argument('--test', '-t', action=argparse.BooleanOptionalAction, help='Activate the test mode')
+    parser.add_argument('--debug', '-d', action=argparse.BooleanOptionalAction, help='Activate the debug mode')
     namespace = parser.parse_args(args)
     
-
-    log_level = logging.DEBUG if settings.DEBUG else logging.INFO
-    logging.basicConfig(level=log_level)
+    logging.basicConfig(level=logging.DEBUG if namespace.debug else logging.INFO)
 
     try:
         Database.connect(SQLiteDB, 'database.sqlite3')
-        if namespace.test is True:
+        init()
+        if namespace.test:
             logging.debug('*** Running the test function ***')
             test()
             logging.debug('*** Done with the test function ***')

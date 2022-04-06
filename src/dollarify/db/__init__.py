2b3f64a6276be8fbc3eeca161178bd329af9b1ac
Frederic Perron <frederic.norrep@gmail.com>
2022-04-05 22:50:44 -0400

    added automatic function implementation assignment

--- src/dollarify/db/__init__.py
+++ src/dollarify/db/__init__.py
@@ -3,6 +3,7 @@ import sqlite3
 import os
 from typing import Tuple
 from pathlib import Path
+import inspect
 
 from dollarify.db import sqlite3
 from dollarify.static import staticfiles
@@ -39,10 +40,12 @@ class Database:
         :return: A tuple: (database_connection, database_cursor)
         """
         cls._connect(*args, **kwargs)
-        Database.close = cls.close
-        Database.query = cls.query
-        Database.query_script_file = cls.query_script_file
-        Database.commit = cls.commit
+
+        func_list = [name for name, value in inspect.getmembers(Database, predicate=inspect.isfunction) 
+                    if not name.startswith("__") and not name == Database.connect.__name__]
+        
+        for name in func_list:
+            setattr(Database, name, getattr(cls, name))
         
 
     def commit():
@@ -52,6 +55,25 @@ class Database:
     def close():
         """
         Terminate the connection to the database.
+        """
+        raise NotImplementedError()
+
+    
+    def insert_one():
+        """
+        Insert a row into a table.
+        """
+        raise NotImplementedError()
+
+    def select_one():
+        """
+        Select a row from a table.
+        """
+        raise NotImplementedError()
+
+    def update_one():
+        """
+        Update a row into a table.
         """
         raise NotImplementedError()
 
@@ -87,6 +109,35 @@ class SQLiteDB(Database):
 
     def commit():
         Database.CONNECTION.commit()
+
+    def insert_one(table: str, columns: tuple, task: tuple, commit=True):
+        values = (len(task) * '?,').strip(',')
+        Database.query(f"INSERT INTO {table} ({','.join(columns)}) VALUES ({values});", task, commit=commit)
+    
+    def select_one(table: str, pk: str, columns: str = '*'):
+        Database.query(f'SELECT {columns} FROM {table} WHERE uuid=?;', task=(pk,))
+        response = Database.CURSOR.fetchone()
+        if response is None or len(response) == 0:
+            raise ValueError(f"The model with the uuid {pk} was not found in the table {table}")
+        return response
+
+    def update_one(table: str, columns: tuple, task: tuple, pk_col: str, commit=True):
+        """
+        Update a row in the database.
+        task:   The values to replace the columns old values. 
+                The value of the primary key should be added at the end of the task.
+        pk_col: The name of the primary key column.
+        """
+        # create a column placeholder for the values: col1=?,col2=?,col3=?
+        columns_str = ''
+        for col in columns:
+            columns_str += f"{col}=?,"
+        columns_str = columns_str.strip(',')
+        Database.query(f"UPDATE {table} SET {columns_str} WHERE {pk_col}=?", task, commit=commit)
+    
+
+
+    
             
 
 # Specific Dollarify DB Management #

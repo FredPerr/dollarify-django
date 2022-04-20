7bac3817f836b3c801ccbd177fd87e8df833cd34
Frederic Perron <frederic.norrep@gmail.com>
2022-04-19 20:09:03 -0400

    added the command run script

---
+++ src/dollarify/scripts/run.py
@@ -0,0 +1,25 @@
+import os
+
+import click
+
+from dollarify.utils import config
+from dollarify import dollarify
+
+
+@click.command()
+@click.option('-d', '--debug', is_flag=True, default=False, help='Active the debug mode while running')
+@click.option('-p', '--production', is_flag=True, default=False, help='Active the production mode while running')
+def cli(debug, production):
+
+    postgresql_config = config.load('config.ini', 'postgresql')
+    flask_config = config.load('config.ini', 'flask')
+
+    if not production:
+        os.environ['FLASK_ENV'] = 'development'
+
+    dollarify.app = dollarify.create_app()
+    dollarify.app.run(debug=debug)
+
+    
+
+    
